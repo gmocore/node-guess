80b52ed8f917d193ae71aa47d900ace9926b4949
Black <gmocore@gmail.com>
2019-09-16 15:26:54 -0600

    aded blank replacement in letter function

--- assets/js/Letter.js
+++ assets/js/Letter.js
@@ -4,15 +4,14 @@ const Letter = function() {
   this.word = index.wordSelection();
   this.guess = process.argv[2];
   this.splitLetters = this.word.split("");
-  this.wordBlank = "";
+  this.wordBlank = [];
   this.remainingGuesses = this.splitLetters.length;
   for (var position in this.splitLetters) {
-    this.wordBlank += "_ ";
+    this.wordBlank[position] = "_ ";
   }
-  this.wordBlank[3] = "x";
   for (let i = 0; i < this.splitLetters.length; i++) {
     if (this.splitLetters[i] === this.guess) {
-      this.wordBlank[i] = this.guess;
+      this.wordBlank[i] = this.guess.toString();
       this.remainingGuesses--;
     }
   }
